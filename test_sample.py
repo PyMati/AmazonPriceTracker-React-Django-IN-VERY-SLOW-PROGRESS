cca7e7efa6731c1f13d917c516269265ac691b7d
PyMati <mateuszcodedeveloper@gmail.com>
2022-11-14 21:53:48 +0100

    little changes.

--- test_sample.py
+++ test_sample.py
@@ -1,9 +1,20 @@
 "Testing functionality of workers."
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 import pytest
-import worker, worker_creator, file_reader, journalist
+import file_reader
+import journalist
+import worker
+import worker_creator
+
 
 def create_worker():
-    worker.Observer("URL", "PRICE", "ITEM")
+    try:
+        return worker.Observer("URL", "PRICE", "ITEM")
+    except:
+        return None
 
 def test_create_worker():
-    assert create_worker != None
+    assert create_worker is not None
+    
