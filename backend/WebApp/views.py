c252ecbfccb811343149846a7d4323d7dc9ba4a1
PyMati <mateuszcodedeveloper@gmail.com>
2022-11-19 23:38:26 +0100

    Proper set up of backend

--- backend/WebApp/views.py
+++ backend/WebApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hejoooo")
+def index(request):
+    return HttpResponse("Welcome!")
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""pricetrackerback URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('appbackend.urls'))
+    path('backend/', include("WebApp.urls"))
 ]
