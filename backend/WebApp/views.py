8593ed09b88a6b101384a4b6b9246c2bfa7c1fc3
PyMati <mateuszcodedeveloper@gmail.com>
2022-11-28 22:14:13 +0100

    Clearing

--- backend/WebApp/views.py
+++ backend/WebApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template import loader
+from WebApp.models import User, Admin_User
+from WebApp.serializers import User_Serializer, Admin_Serializer
+from rest_framework import viewsets
 
-
+# Admin website.
 def index(request):
-    template = loader.get_template("./ASite.html")
+    template = loader.get_template("./index.html")
     ls = []
     for n in range(11):
         ls.append(1)
@@ -12,3 +15,5 @@ def index(request):
         "ln": ls
     }
     return HttpResponse(template.render(context, request))
+
+# Api Classes and Configuration.
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_api_framework'
+    'rest_framework_'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
