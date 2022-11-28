0c6273c181a0b01c4a26870265704fca671ba3d2
PyMati <mateuszcodedeveloper@gmail.com>
2022-11-28 20:48:41 +0100

    Adding Bootstrap.

--- backend/WebApp/models.py
+++ backend/WebApp/models.py
@@ -5,3 +5,8 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
+    
+
+class Admin_User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
