c9e7982705a48eb1f64c6daf4d386d918264b7e0
PyMati <mateuszcodedeveloper@gmail.com>
2022-12-02 00:24:35 +0100

    Adding simple forms/ Icons.

---
+++ frontend/pricetrackerfront/src/react-pages/register_page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import RegisterForm from "../forms/register_form.js";
+import Footer from "../base/footer.js";
+import Navbar from "../base/navbar.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class RegisterPage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div></div>
+                <Navbar />
+                <RegisterForm />
+                <Footer />
+            </div>
+        )
+    }
+}
